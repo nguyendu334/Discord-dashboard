b860c2906c18a0597bf1831f9bd5f27eb0f3616f
_nguyen.du <1851061329@e.tlu.edu.vn>
2023-03-20 14:08:36 +0700

    Protect Next Routes

--- src/controllers/guilds/index.ts
+++ src/controllers/guilds/index.ts
@@ -1,12 +1,29 @@
 import { Request, Response } from 'express';
 import { User } from '../../database/schemas/User';
-import { getBotGuildsService, getMutuaGuildsService, getUserGuildsService } from '../../services/guilds';
+import {
+    getBotGuildsService,
+    getMutuaGuildsService,
+    getUserGuildsService,
+} from '../../services/guilds';
 
 export async function getGuildsController(req: Request, res: Response) {
     const user = req.user as User;
     try {
         const guilds = await getMutuaGuildsService(user.id);
-        res.send({ guilds });
+        res.send(guilds);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({ msg: 'Error' });
+    }
+}
+
+export async function getGuildPermissionsController(req: Request, res: Response) {
+    const user = req.user as User;
+    const { id } = req.params;
+    try {
+        const guilds = await getMutuaGuildsService(user.id);
+        const valid = guilds.some((guild) => guild.id === id);
+        return valid ? res.sendStatus(200) : res.sendStatus(403);
     } catch (error) {
         console.log(error);
         res.status(400).send({ msg: 'Error' });
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -4,7 +4,7 @@ import passport from 'passport';
 const router = Router();
 
 router.get('/discord', passport.authenticate('discord'), (req, res) => res.send(200));
-router.get('/discord/redirect', passport.authenticate('discord'), (req, res) => res.send({ msg: 'success' }));
+router.get('/discord/redirect', passport.authenticate('discord'), (req, res) => res.redirect('http://localhost:3000/menu'));
 router.get('/status', (req, res) => {
     if(req.user) {
         return req.user ? res.send(req.user) : res.status(401).send({
